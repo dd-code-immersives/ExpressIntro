caa89c960820960bb07f63f6b633663eff466f32
dd-code-immersives <domilamad89@gmail.com>
2023-01-30 12:31:11 -0500

    changed the port number

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
  	const express = require('express')
 
  	const app = express()
- 	const port = 3000
+ 	const port = 3001
 
  	app.use(express.json()) // This line is necessary for Express to be able to parse JSON in request body's
 
